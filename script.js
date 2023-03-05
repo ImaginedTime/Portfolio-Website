8ca298fe215c4871638d6818ba3fdbb2ecedf2ea
Uday <udayomsrivastava111@gmail.com>
2023-03-05 09:28:38 +0530

    made the website

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+        $(".nav-links").slideToggle();
+    });
+});
+
+// // to check whether the media query detects properly
+// setInterval(check, 50);
+// function check(){
+//     const mediaQuery = window.matchMedia('( max-width: 768px )')
+//     // Note the `matches` property
+//     if ( mediaQuery.matches ) {
+//     console.log('Media Query Matched!')
+//     }
+// }
